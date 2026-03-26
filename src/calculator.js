5c555e341a7c2d64156e9eb8f641173798a55b38
YuFeng <62492294+maplefeng-a@users.noreply.github.com>
2026-03-26 16:02:13 +0800

    feat: add calculator module

---
+++ src/calculator.js
@@ -0,0 +1,25 @@
+/**
+ * Calculator module with basic arithmetic functions
+ */
+
+/**
+ * Returns the sum of a and b
+ * @param {number} a - First number
+ * @param {number} b - Second number
+ * @returns {number} Sum of a and b
+ */
+function add(a, b) {
+  return a + b;
+}
+
+/**
+ * Returns the product of a and b
+ * @param {number} a - First number
+ * @param {number} b - Second number
+ * @returns {number} Product of a and b
+ */
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = { add, multiply };
